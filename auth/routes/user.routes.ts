316015a8827e6fc41eda92b8a2ff11f3d4fc1330
almas <ntldr@mail.ru>
2024-01-15 21:48:04 +0600

    auth/user - add method  get by id

--- auth/routes/user.routes.ts
+++ auth/routes/user.routes.ts
@@ -1,13 +1,20 @@
 import { NextFunction, Response, Request, Router } from 'express';
-import { UserGetSchema } from '../schemas/user.get.schema';
+import { UserFindSchema, UserGetSchema } from '../schemas/user.get.schema';
 import { validateSchema } from '../middlewares/validateSchema';
-import { UserGetController } from '../modules/user/get.controller';
+import { UserFindController, UserGetController } from '../modules/user/find.controller';
 
 export function UserRoutes() {
   const router = Router();
   //router.use(startTiming);
   router.get(
     '/',
+    (req: Request, res: Response, next: NextFunction) => {
+      validateSchema(req, res, next, UserFindSchema);
+    },
+    UserFindController,
+  );
+  router.get(
+    '/:id',
     (req: Request, res: Response, next: NextFunction) => {
       validateSchema(req, res, next, UserGetSchema);
     },
--- auth/schemas/user.get.schema.ts
+++ auth/schemas/user.get.schema.ts
@@ -1,6 +1,16 @@
 import { z } from 'zod';
 
 export const UserGetSchema = z.object({
+  params: z.object({
+    id: z
+      .string()
+      .transform((val) => Number(val))
+      .pipe(z.number().min(1))
+      .optional(),
+  }),
+});
+
+export const UserFindSchema = z.object({
   query: z.object({
     id: z
       .string()
